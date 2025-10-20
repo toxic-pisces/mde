f8e3b16528afcde6f1022321c67701e91c200d29
GetOnMy33kRolex <niklas.reinig@hs-augsburg.de>
2025-10-20 13:37:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'maschinen-pruefsystem-v1';
+const CACHE_NAME = 'maschinen-pruefsystem-v3';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
