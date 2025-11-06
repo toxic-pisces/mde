bb20e80fb7ad43243da33d99670155ef3caa1c2a
GetOnMy33kRolex <niklas.reinig@hs-augsburg.de>
2025-11-06 08:18:52 +0100

    Update Firebase config with new project credentials
    Updated Firebase configuration with new project values.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,13 +3,13 @@
 // Sie finden diese in der Firebase Console unter: Project Settings > General > Your apps
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDOt2f94DSXcEryqN0ZHrlLPwxQMSG4_V8",
+  authDomain: "mdee-90798.firebaseapp.com",
+  projectId: "mdee-90798",
+  storageBucket: "mdee-90798.firebasestorage.app",
+  messagingSenderId: "102939691135",
+  appId: "1:102939691135:web:dbb91a0a800f94b0c8174a",
+  measurementId: "G-P0M88KFQEP"
 };
 
 // Firebase initialisieren
